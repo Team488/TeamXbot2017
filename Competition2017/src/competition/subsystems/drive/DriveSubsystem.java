980e21a4cc8976f7dce41adb7fa7bd4bc540de9e
Issa Mathno <imathno@hotmail.com>
2017-01-24 19:16:36 -0800

    Feature/clean up summer drive (#2)
    * -Added CTRE to class path so talons can work
    -Added a new tankDrive method tankDrivePowerMode()
    -Updated tankDriveWithJoySticks
    
    * Refractor code to work on 2017 framework
    
    * Removed comments that conflicted with check style
    
    * Updated variables names and comments
    
    * Added new comments and refracctor PID variable names to be more specific.

--- Competition2017/src/competition/subsystems/drive/DriveSubsystem.java
+++ Competition2017/src/competition/subsystems/drive/DriveSubsystem.java
@@ -5,37 +5,164 @@ import org.apache.log4j.Logger;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
 import xbot.common.command.BaseSubsystem;
-import xbot.common.controls.actuators.XSpeedController;
+import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.injection.wpi_factories.WPIFactory;
+import xbot.common.math.MathUtils;
+import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
 
 @Singleton
 public class DriveSubsystem extends BaseSubsystem {
     private static Logger log = Logger.getLogger(DriveSubsystem.class);
 
-    public final XSpeedController leftFrontDrive;
-    public final XSpeedController leftRearDrive;
-    public final XSpeedController rightFrontDrive;
-    public final XSpeedController rightRearDrive;
+    public final XCANTalon leftDrive;
+    public final XCANTalon leftDriveSlave;
+    public final XCANTalon rightDrive;
+    public final XCANTalon rightDriveSlave;
+    
+    private final DoubleProperty encoderCodesProperty;
+    private final DoubleProperty maxSpeedProperty;
+    private final DoubleProperty pVelProp;
+    private final DoubleProperty iVelprop;
+    private final DoubleProperty dVelProp;
+    private final DoubleProperty fVelProp;
 
     @Inject
     public DriveSubsystem(WPIFactory factory, XPropertyManager propManager) {
         log.info("Creating DriveSubsystem");
 
-        this.leftFrontDrive = factory.getSpeedController(0);
-        this.leftRearDrive = factory.getSpeedController(2);
+        // TODO: Update these defaults. The current values are blind guesses.
+        encoderCodesProperty = propManager.createPersistentProperty("Drive encoder codes per rev", 512);
+        maxSpeedProperty = propManager.createPersistentProperty("Max drive motor speed (rotations per second)", 5);
 
-        this.rightFrontDrive = factory.getSpeedController(1);
-        this.rightFrontDrive.setInverted(true);
-        this.rightRearDrive = factory.getSpeedController(3);
-        this.rightRearDrive.setInverted(true);
+        pVelProp = propManager.createPersistentProperty("Drive Vel P", 2);
+        iVelprop = propManager.createPersistentProperty("Drive Vel I", 0);
+        dVelProp = propManager.createPersistentProperty("Drive Vel D", -100);
+        fVelProp = propManager.createPersistentProperty("Drive Vel F", 0);
+        
+        this.leftDrive = factory.getCANTalonSpeedController(3);
+        this.leftDriveSlave = factory.getCANTalonSpeedController(4);
+        configMotorTeam(leftDrive, leftDriveSlave);
+        leftDrive.createTelemetryProperties("Left master", propManager);
+        leftDriveSlave.createTelemetryProperties("Left slave", propManager);
+        
+        this.rightDrive = factory.getCANTalonSpeedController(1);
+        this.rightDriveSlave = factory.getCANTalonSpeedController(2);
+        configMotorTeam(rightDrive, rightDriveSlave);
+        rightDrive.createTelemetryProperties("Right master", propManager);
+        rightDriveSlave.createTelemetryProperties("Right slave", propManager);
     }
 
-    public void tankDrive(double leftPower, double rightPower) {
-        this.leftFrontDrive.set(leftPower);
-        this.leftRearDrive.set(leftPower);
-        this.rightFrontDrive.set(rightPower);
-        this.rightRearDrive.set(rightPower);
+    private void configMotorTeam(XCANTalon master, XCANTalon slave) {
+        // TODO: Check faults and voltage/temp/current
+        
+        // Master config
+        master.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+        master.setBrakeEnableDuringNeutral(false);
+        master.reverseSensor(false);
+        master.enableLimitSwitches(false, false);
+        
+        master.configNominalOutputVoltage(0,  -0);
+        master.configPeakOutputVoltage(12, -12);
+
+        master.setProfile(0);
+        master.setF(0);
+        updateMotorConfig(master);
+        master.setControlMode(TalonControlMode.Speed);
+        
+        master.set(0);
+        
+        // Slave config
+        slave.configNominalOutputVoltage(0,  -0);
+        slave.configPeakOutputVoltage(12, -12);
+        slave.enableLimitSwitches(false, false);
+        
+        slave.setControlMode(TalonControlMode.Follower);
+        slave.set(master.getDeviceID());
+    }
+    
+    /**
+     * Converts joystick input into a velocity goal
+     * @param power the joystick value that is being passed through
+     * @return the velocity goal
+     */
+    public double convertPowerToVelocityTarget(double power) {
+        double maxTicksPerTenMs = maxSpeedProperty.get() * encoderCodesProperty.get() / 100;
+        return power * maxTicksPerTenMs;
+    }
+    
+    private void ensureModeForTalon(XCANTalon talon, TalonControlMode mode) {
+        if (talon.getControlMode() != mode) {
+            talon.setControlMode(mode);
+        }
+    }
+    
+    private void ensureSpeedModeForDrive() {
+        ensureModeForTalon(leftDrive, TalonControlMode.Speed);
+        ensureModeForTalon(rightDrive, TalonControlMode.Speed);
+    }
+    
+    private void ensurePowerModeForDrive() {
+        ensureModeForTalon(leftDrive, TalonControlMode.PercentVbus);
+        ensureModeForTalon(rightDrive, TalonControlMode.PercentVbus);
+    }
+
+    public void tankDriveVelocityPid(double leftPower, double rightPower) {
+        // TODO: Move parameter updates to something more consistent
+        ensureSpeedModeForDrive();
+        
+        // Coerce powers into appropriate limits
+        leftPower = MathUtils.constrainDoubleToRobotScale(leftPower);
+        rightPower = MathUtils.constrainDoubleToRobotScale(rightPower);
+        
+        updateMotorConfig(leftDrive);
+        updateMotorConfig(rightDrive);
+
+        leftDrive.set(convertPowerToVelocityTarget(leftPower));
+        rightDrive.set(convertPowerToVelocityTarget(rightPower));
+        
+        updateMotorTelemetry();
+    }
+    
+    /**
+     * Simple power based tank drive method of the robot
+     */
+    public void tankDrivePowerMode(double leftPower, double rightPower) {
+        ensurePowerModeForDrive();
+        
+        // Coerce powers into appropriate limits
+        leftPower = MathUtils.constrainDoubleToRobotScale(leftPower);
+        rightPower = MathUtils.constrainDoubleToRobotScale(rightPower);
+       
+        leftDrive.set(leftPower);
+        rightDrive.set(rightPower);
+        
+        updateMotorTelemetry();
+    }
+    
+    /**
+     * Updates the motor with new values without resetting the robot
+     * @param motor used to access the motor to set and give data
+     */
+    private void updateMotorConfig(XCANTalon motor) {
+        motor.configEncoderCodesPerRev((int)encoderCodesProperty.get());
+        motor.setP(pVelProp.get());
+        motor.setI(iVelprop.get());
+        motor.setD(dVelProp.get());
+        motor.setF(fVelProp.get());
+    }
+    
+    /**
+     * Get values from robot to output on the teleop interface
+     * IMPORTANT: When setting power to motors call this method
+     */
+    public void updateMotorTelemetry() {
+        leftDrive.updateTelemetryProperties();
+        leftDriveSlave.updateTelemetryProperties();
+        rightDrive.updateTelemetryProperties();
+        rightDriveSlave.updateTelemetryProperties();
     }
 }
