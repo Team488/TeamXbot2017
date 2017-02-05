8cc82648c0c80ec25b8910e96ea349ac1b58fab7
Issa Mathno <imathno@hotmail.com>
2017-02-04 19:10:58 -0800

    Added Pneutmatic brake subsystem + test for it (#23)
    Adds a brake that automatically engages/disengages as the climbing winch is activated/stopped.

--- Competition2017/src/competition/subsystems/climbing/ClimbingSubsystem.java
+++ Competition2017/src/competition/subsystems/climbing/ClimbingSubsystem.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANTalon;
+import xbot.common.controls.actuators.XSolenoid;
 import xbot.common.injection.wpi_factories.WPIFactory;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.XPropertyManager;
@@ -17,6 +18,8 @@ public class ClimbingSubsystem extends BaseSubsystem {
     private final DoubleProperty ascendPowerProperty;
     private final DoubleProperty descendPowerProperty;
     private final XCANTalon climbingMotor;
+    
+    public final XSolenoid brakeSolenoid;
 
     @Inject
     public ClimbingSubsystem(WPIFactory factory, XPropertyManager propManager){
@@ -27,19 +30,28 @@ public class ClimbingSubsystem extends BaseSubsystem {
         climbingMotor.setBrakeEnableDuringNeutral(true);
         climbingMotor.setControlMode(TalonControlMode.PercentVbus);
 
+        brakeSolenoid = factory.getSolenoid(3);
+        
         descendPowerProperty = propManager.createPersistentProperty("Climber descend power", -0.5);
         ascendPowerProperty = propManager.createPersistentProperty("Climber ascend power", 0.5);
     }
 
     public void stop() {
         climbingMotor.set(0);
+        brakeSolenoid.set(true);
     }
 
     public void ascend() {
         climbingMotor.set(ascendPowerProperty.get());
+        brakeSolenoid.set(false);
     }
 
     public void descend() {
         climbingMotor.set(descendPowerProperty.get());
+        brakeSolenoid.set(false);
+    }
+    
+    public void setBrake(boolean brakeOn) {
+        brakeSolenoid.set(brakeOn);
     }
 }
