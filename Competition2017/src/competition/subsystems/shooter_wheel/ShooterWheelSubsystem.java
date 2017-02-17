5115506f50e71bf1d1829b857c11b9f66d23ef2c
imathno <imathno@hotmail.com>
2017-02-16 20:49:47 -0800

    Added a shooter wheel database

--- Competition2017/src/competition/subsystems/shooter_wheel/ShooterWheelSubsystem.java
+++ Competition2017/src/competition/subsystems/shooter_wheel/ShooterWheelSubsystem.java
@@ -1,5 +1,8 @@
 package competition.subsystems.shooter_wheel;
 
+import com.db.influxdb.Configuration;
+import com.db.influxdb.DataWriter;
+
 import competition.subsystems.BaseXCANTalonPairSpeedControlledSubsystem;
 import competition.subsystems.RobotSide;
 import xbot.common.injection.wpi_factories.WPIFactory;
@@ -10,7 +13,10 @@ import xbot.common.properties.XPropertyManager;
 public class ShooterWheelSubsystem extends BaseXCANTalonPairSpeedControlledSubsystem {
     
     private final RobotSide side;
+    
     protected final DoubleProperty flushToBoilerTargetSpeed;
+    
+    protected DataWriter writer;
     
     public enum TypicalShootingPosition {
         FlushToBoiler
@@ -40,6 +46,14 @@ public class ShooterWheelSubsystem extends BaseXCANTalonPairSpeedControlledSubsy
         this.side = side;
         flushToBoilerTargetSpeed = 
                 propManager.createPersistentProperty(side + " flush to boiler target speed", 3500);
+        Configuration config = new Configuration("localhost", "8086", "", "", "test");
+        try {
+            this.writer = new DataWriter(config);
+            writer.setMeasurement("ShooterWheelSubsystem");
+            writer.addTag("Side", side.toString());
+        } catch (Exception e) {
+            this.writer = null;
+        }
     }
     
     public RobotSide getSide(){
@@ -64,6 +78,21 @@ public class ShooterWheelSubsystem extends BaseXCANTalonPairSpeedControlledSubsy
         // (flush to boiler, one robot width, some other range...)
         // For now, it's just this one range.
         setTargetSpeed(flushToBoilerTargetSpeed.get());
+    }
+    
+    @Override
+    public void updatePeriodicData() {
+        super.updatePeriodicData();
+        if (writer != null) {
+            writer.addField("OutputPower", systemOutputPower.get());
+            writer.addField("TalonError", systemTalonError.get());
+            writer.addField("CurrentSpeed", systemCurrentSpeed.get());
+            try {
+                writer.writeData();
+            } catch (Exception e) {
+            
+            }
+        }
     }
 }
 
