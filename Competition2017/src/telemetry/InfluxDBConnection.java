dbbfba6acd29cddb8c855f415f674d4548f8c426
Alex Schokking <aschokking@gmail.com>
2017-02-20 20:04:19 -0800

    Initial proof of concept working

--- Competition2017/src/telemetry/InfluxDBConnection.java
+++ Competition2017/src/telemetry/InfluxDBConnection.java
@@ -9,21 +9,40 @@ import org.influxdb.dto.Point;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import xbot.common.properties.BooleanProperty;
+import xbot.common.properties.StringProperty;
+import xbot.common.properties.XPropertyManager;
+
 @Singleton
 public class InfluxDBConnection {
 
+    protected final BooleanProperty isEnabledProp;
+    protected final StringProperty influxAddressProp;
+    
     protected final InfluxDB influxDB;
     protected final String dbName = "XbotRobotData";
     
     @Inject
-    public InfluxDBConnection() {
-        influxDB = InfluxDBFactory.connect("http://10.4.88.100:8086", "root", "root");
+    public InfluxDBConnection(XPropertyManager propMan) {
+        isEnabledProp = propMan.createPersistentProperty("InfluxDB logging enabled", false);
+        influxAddressProp = propMan.createPersistentProperty("Influx address", "http://10.4.88.100:8086");
         
-        // Flush every 2000 Points, at least every 1000ms
-        influxDB.enableBatch(2000, 1000, TimeUnit.MILLISECONDS);
+        if(isEnabledProp.get()) {
+            // current raspberrypi static ip
+            influxDB = InfluxDBFactory.connect(influxAddressProp.get(), "root", "root");
+            
+            int flushEveryPointsCount = 2000;
+            int flushEveryMS = 1000;
+            influxDB.enableBatch(flushEveryPointsCount, flushEveryMS, TimeUnit.MILLISECONDS);
+        } else {
+            influxDB = null;
+        }
     }
     
     public void writePoint(Point point) {
-        influxDB.write(dbName, "autogen", point);
+        if(influxDB != null && isEnabledProp.get()) {
+            // autogen is the default retention policy
+            influxDB.write(dbName, "autogen", point);
+        }
     }
 }
