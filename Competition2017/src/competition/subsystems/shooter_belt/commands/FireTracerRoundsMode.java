ebecdf32a597db14cc854310375cbc596493d21e
SethNguyen <sensaiwu10@gmail.com>
2017-03-25 11:50:14 -0800

    Unfinished "Fire Tracer" mode code.

---
+++ Competition2017/src/competition/subsystems/shooter_belt/commands/FireTracerRoundsMode.java
@@ -0,0 +1,27 @@
+package competition.subsystems.shooter_belt.commands;
+
+import competition.subsystems.shooter_belt.ShooterBeltSubsystem;
+import competition.subsystems.shooter_wheel.ShooterWheelSubsystem;
+
+public class FireTracerRoundsMode extends BaseShooterBeltCommand {
+    protected final ShooterWheelSubsystem shooterWheelSubsystem;
+
+    public FireTracerRoundsMode(ShooterBeltSubsystem shooterBeltSubsystem, ShooterWheelSubsystem shooterWheelSubsystem) {
+        super(shooterBeltSubsystem);
+        this.shooterWheelSubsystem = shooterWheelSubsystem;
+    }
+    
+    @Override
+    public void initialize() {
+        log.info("Initializing");
+    }
+
+    @Override
+    public void execute() {
+        if(shooterWheelSubsystem.isAtSpeed()){
+            shooterBeltSubsystem.intakeUsingSpeed();
+        } else {
+            shooterBeltSubsystem.setPower(0);
+        }
+    }  
+}
