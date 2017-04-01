90c077edc15b24961dfdb6357670ed833cec82c6
Issa Mathno <imathno@hotmail.com>
2017-03-31 20:10:29 -0700

    Fix autonomous to wait for shooter wheel to be at speed  (#131)
    * Fix autonomus to wait for shooter wheel
    
    * Removed unused param
    
    * Fix conventions
    
    * Conventions
    
    * Fix isWheelAtSpeed logic
    
    * Fix checkstyle

--- Competition2017/src/competition/subsystems/shoot_fuel/EndToEndShootingCommandGroup.java
+++ Competition2017/src/competition/subsystems/shoot_fuel/EndToEndShootingCommandGroup.java
@@ -2,17 +2,13 @@ package competition.subsystems.shoot_fuel;
 
 import com.google.inject.Inject;
 
-import competition.subsystems.agitator.AgitatorsManagerSubsystem;
 import competition.subsystems.collector.commands.IntakeCollectorCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import xbot.common.properties.XPropertyManager;
 
-public class EndtoEndShootingCommandGroup extends CommandGroup {
+public class EndToEndShootingCommandGroup extends CommandGroup {
     
     @Inject
-    public EndtoEndShootingCommandGroup(XPropertyManager propManager, 
-            AgitatorsManagerSubsystem agitatorsManagerSubsystem,
-            LeftShootFuelAndAgitateCommandGroup left,
+    public EndToEndShootingCommandGroup(LeftShootFuelAndAgitateCommandGroup left,
             RightShootFuelAndAgitateCommandGroup right,
             IntakeCollectorCommand intake) {
                 
