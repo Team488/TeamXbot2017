98f3546fafc609bcb8db156d1b0f7f6a95382fe1
imathno <imathno@hotmail.com>
2017-04-03 20:55:28 -0700

    Took a stab at fixing the conflicts

--- Competition2017/src/competition/subsystems/shoot_fuel/LeftShootFuelCommandGroup.java
+++ Competition2017/src/competition/subsystems/shoot_fuel/LeftShootFuelCommandGroup.java
@@ -2,19 +2,21 @@ package competition.subsystems.shoot_fuel;
 
 import com.google.inject.Inject;
 
+import competition.subsystems.agitator.AgitatorsManagerSubsystem;
 import competition.subsystems.shooter_belt.ShooterBeltsManagerSubsystem;
 import competition.subsystems.shooter_belt.commands.RunBeltIfWheelAtSpeedCommand;
 import competition.subsystems.shooter_wheel.ShooterWheelSubsystem.TypicalShootingPosition;
 import competition.subsystems.shooter_wheel.ShooterWheelsManagerSubsystem;
 import competition.subsystems.shooter_wheel.commands.RunShooterWheelsForRangeCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import xbot.common.properties.XPropertyManager;
 
 public class LeftShootFuelCommandGroup extends CommandGroup {
     
     @Inject
     public LeftShootFuelCommandGroup(XPropertyManager propManager, 
             AgitatorsManagerSubsystem agitatorsManagerSubsystem,
-    public LeftShootFuelCommandGroup(ShooterWheelsManagerSubsystem shooterWheelsManagerSubsystem,
+            ShooterWheelsManagerSubsystem shooterWheelsManagerSubsystem,
             ShooterBeltsManagerSubsystem shooterBeltsManagerSubsystem) {
         RunShooterWheelsForRangeCommand runWheel =
                 new RunShooterWheelsForRangeCommand(
--- Competition2017/src/competition/subsystems/shoot_fuel/RightShootFuelCommandGroup.java
+++ Competition2017/src/competition/subsystems/shoot_fuel/RightShootFuelCommandGroup.java
@@ -2,19 +2,21 @@ package competition.subsystems.shoot_fuel;
 
 import com.google.inject.Inject;
 
+import competition.subsystems.agitator.AgitatorsManagerSubsystem;
 import competition.subsystems.shooter_belt.ShooterBeltsManagerSubsystem;
 import competition.subsystems.shooter_belt.commands.RunBeltIfWheelAtSpeedCommand;
 import competition.subsystems.shooter_wheel.ShooterWheelsManagerSubsystem;
 import competition.subsystems.shooter_wheel.ShooterWheelSubsystem.TypicalShootingPosition;
 import competition.subsystems.shooter_wheel.commands.RunShooterWheelsForRangeCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import xbot.common.properties.XPropertyManager;
 
 public class RightShootFuelCommandGroup extends CommandGroup {
 
     @Inject
     public RightShootFuelCommandGroup(XPropertyManager propManager, 
             AgitatorsManagerSubsystem agitatorsManagerSubsystem,
-    public RightShootFuelCommandGroup(ShooterWheelsManagerSubsystem shooterWheelsManagerSubsystem,
+            ShooterWheelsManagerSubsystem shooterWheelsManagerSubsystem,
             ShooterBeltsManagerSubsystem shooterBeltsManagerSubsystem) {
         RunShooterWheelsForRangeCommand runWheel =
                 new RunShooterWheelsForRangeCommand(
