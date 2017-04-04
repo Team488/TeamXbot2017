d6dd62a3efb3cbc2acbc2e85b45c4e30804c561f
imathno <imathno@hotmail.com>
2017-04-04 11:06:33 -0700

    Added a time threshold

--- Competition2017/src/competition/subsystems/agitator/commands/IntakeAgitatorCommand.java
+++ Competition2017/src/competition/subsystems/agitator/commands/IntakeAgitatorCommand.java
@@ -8,11 +8,17 @@ import xbot.common.properties.XPropertyManager;
 
 public class IntakeAgitatorCommand extends BaseAgitatorCommand {
     private AgitatorSubsystem agitatorSubsystem;
-    public DoubleProperty motorCurrentThreshold;
-    private DoubleProperty unjamTimeInterval;
+    
+    protected final DoubleProperty motorCurrentThreshold;
+    protected final DoubleProperty unjamTimeInterval;
+    protected final DoubleProperty minSecForAgitatorToEject;
+    
     private double mostRecentEjectTime;
     private double currentTime;
     private boolean mostRecentEjectTimeSet = false;
+    
+    private boolean agitatorCanEnable = false;
+    private double agitatorPassedThresholdTime;
     
     public IntakeAgitatorCommand(AgitatorSubsystem agitatorSubsystem, XPropertyManager propManager) {
         super(agitatorSubsystem);
@@ -20,6 +26,8 @@ public class IntakeAgitatorCommand extends BaseAgitatorCommand {
         motorCurrentThreshold = propManager
                 .createPersistentProperty("Threshold for agitator motor current before unjamming", 10);
         unjamTimeInterval = propManager.createPersistentProperty(" time interval for unjamming agitator", 13);
+        minSecForAgitatorToEject = propManager
+                .createPersistentProperty("Minimum seconds after agitator is pass the threshold to run", 1);
     }
     
     @Override
@@ -31,14 +39,32 @@ public class IntakeAgitatorCommand extends BaseAgitatorCommand {
     public void execute(){
         currentTime = Timer.getFPGATimestamp();
         if (agitatorSubsystem.getMotorCurrent() >= motorCurrentThreshold.get()) {
-            mostRecentEjectTimeSet = true;
-            mostRecentEjectTime = Timer.getFPGATimestamp();
-            agitatorSubsystem.eject();
+            toggleTimerSinceMotorPassedThreshold();
+            if (agitatorCanEnable) {
+                mostRecentEjectTimeSet = true;
+                mostRecentEjectTime = Timer.getFPGATimestamp();
+                agitatorSubsystem.eject();
+                resetAgitatorPassedThresholdTime();
+            }
         } else if (mostRecentEjectTimeSet && (currentTime - mostRecentEjectTime) < unjamTimeInterval.get()) {
             agitatorSubsystem.eject();
         } else {
             mostRecentEjectTimeSet = false;
             agitatorSubsystem.intake();
         }
-    }    
+    }
+    
+    private void toggleTimerSinceMotorPassedThreshold() {
+        if (!agitatorCanEnable && agitatorPassedThresholdTime <= 0) {
+            agitatorPassedThresholdTime = Timer.getFPGATimestamp();
+        } else if (currentTime - agitatorPassedThresholdTime <= minSecForAgitatorToEject.get()) {
+            agitatorCanEnable = true;
+        } else {
+            agitatorCanEnable = false;
+        }
+    }
+    
+    private void resetAgitatorPassedThresholdTime() {
+        agitatorPassedThresholdTime = 0;
+    }
 }
